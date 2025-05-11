546d68bfc0bb5867da09c5d14fdc9e43871926c3
alvarorichard <alvaro10048@protonmail.com>
2025-05-11 00:41:03 -0300

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import type React from "react"
+import React from "react";
+import { JSX } from "react";
 import "@/app/globals.css"
-import { Inter } from "next/font/google"
+import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
 import { LanguageProviderWrapper } from "@/components/language-provider-wrapper"
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "GoAnime - Player de Anime para Terminal",
@@ -20,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <LanguageProviderWrapper>{children}</LanguageProviderWrapper>
